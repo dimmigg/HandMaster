d599f2c88a69d9709cc3257efb53db06ea81f1af
dimmigg@gmail.com <dimmigg@gmail.com>
2022-08-24 18:18:01 +0300

    test

--- HandMaster/Startup.cs
+++ HandMaster/Startup.cs
@@ -46,7 +46,6 @@ namespace HandMaster
             });
             services.AddHealthChecks();
             services.AddControllersWithViews();
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
