6246f484ab11c0e8fc2e1ca3138687bb4cbdf5bb
dimmigg@gmail.com <dimmigg@gmail.com>
2022-08-26 08:29:12 +0300

    Добавлена работа с заказами

--- HandMaster/Controllers/InquiryController.cs
+++ HandMaster/Controllers/InquiryController.cs
@@ -1,9 +1,14 @@
 ﻿using HandMaster_DataAccess.Repository.IRepository;
+using HandMaster_Models;
 using HandMaster_Models.ViewModels;
+using HandMaster_Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace HandMaster.Controllers
 {
+    [Authorize(WC.AdminRole)]
     public class InquiryController : Controller
     {
         private readonly IInquiryHeaderRepository _inqHRepo;
@@ -32,6 +37,42 @@ namespace HandMaster.Controllers
             };
 
             return View(InquiryVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            InquiryVM.InquiryDetail = _inqDRepo.GetAll(x => x.InquiryHeaderId == InquiryVM.InquiryHeader.Id);
+
+            foreach (var detail in InquiryVM.InquiryDetail)
+            {
+                ShoppingCart shoppingCart = new ShoppingCart()
+                {
+                    ProductId = detail.ProductId
+                };
+                shoppingCartList.Add(shoppingCart);
+            }
+            HttpContext.Session.Clear();
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            HttpContext.Session.Set(WC.SessionInquiryId, InquiryVM.InquiryHeader.Id);
+
+            return RedirectToAction("Index", "Cart");
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            InquiryHeader inquiryHeader = _inqHRepo.FirstOrDefault(x => x.Id == InquiryVM.InquiryHeader.Id);
+            IEnumerable<InquiryDetail> inquiryDetails = _inqDRepo.GetAll(x => x.InquiryHeaderId == InquiryVM.InquiryHeader.Id);
+
+            
+            _inqDRepo.RemoveRange(inquiryDetails);
+            _inqHRepo.Remove(inquiryHeader);
+            _inqHRepo.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
         #region API CALLS
--- HandMaster_DataAccess/Repository/Repository.cs
+++ HandMaster_DataAccess/Repository/Repository.cs
@@ -80,6 +80,11 @@ namespace HandMaster_DataAccess.Repository
             dbSet.Remove(entity);
         }
 
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
         public void Save()
         {
             _db.SaveChanges();
